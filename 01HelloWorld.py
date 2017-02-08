7c4c16bc2cc9325f98a2f2d80328934f5760d259
monkrobot <sdenisov93@mail.ru>
2017-02-08 18:29:09 +0300

    Testing of Bresenham's algorithm

--- 01HelloWorld.py
+++ 01HelloWorld.py
@@ -1,30 +1,27 @@
-#n = [[2, 3], [3, 1], [1, 2, 4, 5], [3], [3], [7], [6]]
-#group_a = []
-#group_b = []
-#i = 0
-#
-#while i < len(n)-1:
-## for i in range(0,len(n)-1):
-#    for j in range(1,len(n[i])+1):
-#        for q in range(0, len(n[n[i][j-1]-1])):
-#            print('this is i: ', i)
-#            print('this is j: ', j)
-#            print('this is q: ', q)
-#            print('this is n[i] n['+str(i)+']: '+str(n[i]))
-#            print('this is n[i][j - 1] n[' + str(i) + ']['+str(j-1)+']: ' + str(n[i][j - 1]))
-#            print('this is n[n[i][j - 1]-1][q]: '+str(n[n[i][j - 1] - 1][q]))
-#
-#            if i+1 != n[n[i][j - 1]-1][q]:
-#                print('no')
-#            else:
-#                print('good')
-#                group_a.append(n[i])
-#                group_b.append(n[n[i][j - 1] - 1])
-#                i += 1
-#                continue
-#            i += 1
-#print('group_a',group_a)
-#print('group_b',group_b)
+import math
+import matplotlib.pyplot as plt
+x = []
+y = []
+def func123(yo, xo, x1):
+    x0 = 0
+    y0 = 0
+    error = 0
+    deltax = abs(x1 - x0)
+    deltay = abs(x1 * math.tan(yo/xo) - y0)
+    deltaerr = deltay
+    ys = y0
 
-print('hello world!')
-print('I hope I did not break git')
+    for x_point in range(x0, x1):
+        x.append(x_point)
+        print('ys', x_point, ys)
+        error = error + deltaerr
+        if 2*error >= deltax:
+            ys = ys-1
+            error = error - deltax
+        y.append(ys)
+
+func123(4, 6, 7)
+print('x', x)
+fig = plt.figure()
+plt.plot(x, y)
+plt.show()
