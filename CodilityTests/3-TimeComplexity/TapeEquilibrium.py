1164a0a9862923dd2185a2197ec265fa6a834d75
monkrobot <sdenisov93@mail.ru>
2017-04-27 00:58:44 +0300

    TakeEqulibrium.py

--- CodilityTests/3-TimeComplexity/TapeEquilibrium.py
+++ CodilityTests/3-TimeComplexity/TapeEquilibrium.py
@@ -12,21 +12,20 @@
 #    return minimal
 
 def solution(A):
-    minimal = sum(A)
+
+    minimal = sum(A) #not equal to sum(A). It's a mistake in list with numbers lower than 0
     for P in range(len(A)-1):
         print(A[:(P+1)])
+        print(sum(A[:(P + 1)]))
         print(A[(P+1):])
-        if sum(A[:(P + 1)]) - sum(A[(P + 1):]) > 0:
-            B = sum(A[:(P + 1)]) - sum(A[(P + 1):])
-        else:
-            B = sum(A[(P + 1):]) - sum(A[:(P + 1)])
-
-        if minimal > B:
-            minimal = B
-            print('minimal',B)
+        print('summa all', sum(A[:(P + 1)]) - sum(A[(P + 1):]))
+        if minimal > sum(A[:(P + 1)]) - sum(A[(P + 1):]):
+            minimal = sum(A[:(P + 1)]) - sum(A[(P + 1):])
+            print('minimal', minimal)
         else:
             continue
     return minimal
 
-A = [100, 1, 200, 4, 103]
+A = [100, -1, -8, 5, -10]
 print(solution(A))
+print('sumA', sum(A))
