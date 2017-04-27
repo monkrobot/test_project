c5ad0b39255376c35bb2c787396c2ee4588584b5
monkrobot <sdenisov93@mail.ru>
2017-04-27 08:41:02 +0300

    TakeEqulibrium.py

--- CodilityTests/3-TimeComplexity/TapeEquilibrium.py
+++ CodilityTests/3-TimeComplexity/TapeEquilibrium.py
@@ -1,6 +1,8 @@
+#Find a minimal result of slices sum(A[:(P + 1)]) - sum(A[(P + 1):])
+
 #import math
 #def solution(A):
-#    minimal = sum(A)
+#    minimal = math.fabs(sum(A[:-1]))
 #    for P in range(len(A)-1):
 #        print(A[:(P+1)])
 #        print(A[(P+1):])
@@ -13,19 +15,25 @@
 
 def solution(A):
 
-    minimal = sum(A) #not equal to sum(A). It's a mistake in list with numbers lower than 0
+    minimal = abs(sum(A[:-1]))
+    print('minimal is here:',minimal)
     for P in range(len(A)-1):
         print(A[:(P+1)])
-        print(sum(A[:(P + 1)]))
         print(A[(P+1):])
         print('summa all', sum(A[:(P + 1)]) - sum(A[(P + 1):]))
-        if minimal > sum(A[:(P + 1)]) - sum(A[(P + 1):]):
-            minimal = sum(A[:(P + 1)]) - sum(A[(P + 1):])
+        if minimal > abs(sum(A[:(P + 1)]) - sum(A[(P + 1):])):
+            minimal = abs(sum(A[:(P + 1)]) - sum(A[(P + 1):]))
             print('minimal', minimal)
         else:
             continue
     return minimal
 
-A = [100, -1, -8, 5, -10]
-print(solution(A))
+A = [100, 1, -8, 5, 10]
+print('solution is: ', solution(A))
 print('sumA', sum(A))
+
+
+K = [1,2,3,4]
+for i in range(len(K)-1):
+    print(i+1)
+print('K is', K[:3])
