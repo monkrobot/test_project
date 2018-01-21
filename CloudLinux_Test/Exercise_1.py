bbbb784a3e4eee7edf58babff105dead990041c1
monkrobot <sdenisov93@mail.ru>
2018-01-22 00:02:36 +0300

    CloudLinux_Test

---
+++ CloudLinux_Test/Exercise_1.py
@@ -0,0 +1,38 @@
+A = [3,3,3,3,3,3,2,4,4]
+
+valley = 0
+hill = 0
+
+n = 0
+
+
+#for i in [1, len(A)-1]:
+#    if A[i] == A[i-1]:
+#        continue
+#    elif A[i] > A[i-1] and A[i] > A[i+1]:
+#        hill += 1
+#        print('hill: ', hill)
+#    else:
+#        valley += 1
+#        print('valley: ', valley)
+
+flag = 0
+
+for i in range(len(A)-1):
+    if A[i] == A[i+1]:
+        continue
+    elif A[i] > A[i+1] and flag <= 0:
+        hill += 1
+        n += 1
+        flag = 1
+        print('hill: ', hill)
+    elif A[i] < A[i+1] and flag >= 0:
+        valley += 1
+        n += 1
+        flag = -1
+        print('valley: ', valley)
+
+if A[len(A)-2] != A[len(A)-1] or flag == 0:
+    n += 1
+
+print('n', n)
---
+++ CloudLinux_Test/Exercise_2.py
@@ -0,0 +1,19 @@
+def solution(n):
+    d = [0] * 30
+    l = 0
+    while (n > 0):
+        d[l] = n % 2
+        n //= 2
+        l += 1
+    print(d)
+    for p in range(1, 1 + l):
+        ok = True
+        for i in range(l - p):
+            if d[i] != d[i + p]:
+                ok = False
+                break
+        if ok:
+            return p
+    return -1
+
+print(solution(6))
