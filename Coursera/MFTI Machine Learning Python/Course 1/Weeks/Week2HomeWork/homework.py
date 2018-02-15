0be57657c5db03bfcf04073f39bbfc6951645e4a
monkrobot <sdenisov93@mail.ru>
2018-02-15 17:41:21 +0300

    Machine Learning

--- Coursera/MFTI Machine Learning Python/Course 1/Weeks/Week2HomeWork/homework.py
+++ Coursera/MFTI Machine Learning Python/Course 1/Weeks/Week2HomeWork/homework.py
@@ -1,21 +1,22 @@
-import re
+#import re
+#
+#data = []
+#
+#def check_empty(sentence):
+#    if '' in sentence:
+#        sentence.remove('')
+#        return check_empty(sentence)
+#    else:
+#        return sentence
+#
+#with open('sentences.txt', 'r') as file:
+#    for line in file:
+#        sentence = re.split('[^a-z]', line.lower())
+#        check_empty(sentence)
+#
+#        data.append(sentence)
+#        #for i in sentence:
+#        #    if i == []:
+#        #
+#print(data)
 
-data = []
-
-def check_empty(sentence):
-    if '' in sentence:
-        sentence.remove('')
-        return check_empty(sentence)
-    else:
-        return sentence
-
-with open('sentences.txt', 'r') as file:
-    for line in file:
-        sentence = re.split('[^a-z]', line.lower())
-        check_empty(sentence)
-
-        data.append(sentence)
-        #for i in sentence:
-        #    if i == []:
-        #
-print(data)
