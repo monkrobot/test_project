a6c54a763ac5adcc2e908c74ea664bedeb57d2d7
monkrobot <sdenisov93@mail.ru>
2018-02-21 16:55:44 +0300

    MFTI Machine Learning Python - Week2 - homework

--- Coursera/MFTI Machine Learning Python/Course 1/Weeks/Week2HomeWork/homework.py
+++ Coursera/MFTI Machine Learning Python/Course 1/Weeks/Week2HomeWork/homework.py
@@ -1,6 +1,7 @@
 import re
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import cosine
 
 data = []
 
@@ -34,18 +35,35 @@ for j,k in zip(range(len(words)), words):
     columns[j] = k
 #print(columns)
 
-print(len(columns))
+#print(len(columns))
 
 d = {}
 
 for i in range(len(columns)):
-    d[i] = [False] * len(data)
+    d[i] = [0] * len(data)
 
 for word in range(len(columns)):
     for senten in range(len(data)):
-        if columns[word] in data[senten]:
-            d[word][senten] = True
+        for senten_word in data[senten]:
+            if columns[word] == senten_word:
+                d[word][senten] += 1
 
 frame = pd.DataFrame(d)
 print(frame)
+
+answer1 = []
+
+for senten in range(1, len(data)):
+    answer1.append(cosine(frame.loc[0], frame.loc[senten]))
+print(answer1)
+print(max(answer1))
+sort_answer1 = sorted(answer1)
+print('sort:', sort_answer1)
+print('sort_answer1[-2]', sort_answer1[-2])
+print('sort_answer1[-1]', sort_answer1[-1])
+print('index:', answer1.index(sort_answer1[-1]))
+print('index 0:', answer1[0])
+print('index 16:', answer1[16])
+#print(answer1[16])
+print(data[answer1.index(sort_answer1[-1])])
 
