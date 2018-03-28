67c0ca2d9261b6bdf1b49a436ac72a97da90e0ee
monkrobot <sdenisov93@mail.ru>
2018-03-28 16:24:16 +0300

    JuniorLab - Finish Convert_a_linked_list_to_a_string

--- JuniorLab/Convert_a_linked_list_to_a_string.py
+++ JuniorLab/Convert_a_linked_list_to_a_string.py
@@ -1,41 +1,55 @@
 '''Need to write a function, which returns a string of class Node.data in form: 1 -> 2 -> 8 -> None'''
 
+#class Node:
+#    def __init__(self, data, next='None'):
+#        self.data = data
+#        self.next = next
+#
+#    def return_data(self):
+#        return self.data
+#
+#
+#
+#def stringify(seq, n=''):
+#    if type(seq) == list:
+#        seq = seq[0]
+#
+#    seq_num_dot = seq.find(',')
+#    if seq_num_dot < 0:
+#        ldict = locals()
+#        new_seq = 'm = ' + seq
+#        exec(new_seq)
+#        exec(
+#             'n += str(m.data) + " -> " + str(m.next)\n'
+#             'print("exec n:", n)\n'
+#             'print("str(m.next)", str(m.next))', ldict)
+#        n = ldict['n']
+#        print('n', n)
+#        answer = n
+#        print('answer', answer)
+#        return n
+#    else:
+#        ldict = locals()
+#        new_seq = 'm = ' + seq[:seq_num_dot] + ',' + str([seq[seq_num_dot + 1:][:-1]]) + ')'
+#        exec(new_seq)
+#        exec(
+#             'n += str(m.data) + " -> "\n', ldict)
+#        m,n = [ldict['m'], ldict['n']]
+#        return stringify(m.next, n)
+
 class Node:
-    def __init__(self, data, next='None'):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
-    def return_data(self):
-        return self.data
-
-
-
-def stringify(seq, n=''):
-    if type(seq) == list:
-        seq = seq[0]
-
-    seq_num_dot = seq.find(',')
-    if seq_num_dot < 0:
-        ldict = locals()
-        new_seq = 'm = ' + seq
-        exec(new_seq)
-        exec(
-             'n += str(m.data) + " -> " + str(m.next)\n'
-             'print("exec n:", n)\n'
-             'print("str(m.next)", str(m.next))', ldict)
-        n = ldict['n']
-        print('n', n)
-        answer = n
-        print('answer', answer)
+def stringify(seq):
+    if seq == None:
+        n = 'None'
         return n
-    else:
-        ldict = locals()
-        new_seq = 'm = ' + seq[:seq_num_dot] + ',' + str([seq[seq_num_dot + 1:][:-1]]) + ')'
-        exec(new_seq)
-        exec(
-             'n += str(m.data) + " -> "\n', ldict)
-        m,n = [ldict['m'], ldict['n']]
-        return stringify(m.next, n)
+    m = seq
+    n = str(m.data) + " -> "
+    return n + stringify(m.next)
 
-print(stringify(' Node(1,  Node(2,  Node(3, Node(5, Node(18, Node(134))))))'))
+print(stringify(Node(1,  Node(2,  Node(3, Node(5, Node(18, Node(134))))))))
+print(stringify(None))
 
