4e55f7905fe7a722aee8b2881bb342a2aa18c20b
monkrobot <sdenisov93@mail.ru>
2018-04-23 15:28:09 +0300

    JuniorLab - Greatest_common_divisior.py - works but too slow

--- JuniorLab/Greatest_common_divisor.py
+++ JuniorLab/Greatest_common_divisor.py
@@ -1,26 +1,29 @@
-import time
+#import time
+#
+#n = 1590771464
+#m = 1590771620
+#
+#start = time.time()
+#
+##It works, but too slow
+#def func(n,m):
+#    if n > m:
+#        max_num = n
+#        min_num = m
+#    else:
+#        max_num = m
+#        min_num = n
+#    for i in range(1,min_num+1):
+#        if min_num % i == 0:
+#            max_dev = min_num / i
+#            if max_num % max_dev == 0:
+#                return int(max_dev)
+#        else:
+#            continue
+#
+#print(func(n,m))
+#
+#print(time.time()-start )
 
-n = 1590771464
-m = 1590771620
-
-start = time.time()
-
-#It works, but too slow
-def func(n,m):
-    if n > m:
-        max_num = n
-        min_num = m
-    else:
-        max_num = m
-        min_num = n
-    for i in range(1,min_num+1):
-        if min_num % i == 0:
-            max_dev = min_num / i
-            if max_num % max_dev == 0:
-                return int(max_dev)
-        else:
-            continue
-
-print(func(n,m))
-
-print(time.time()-start )
+import sys
+print(sys.platform)
