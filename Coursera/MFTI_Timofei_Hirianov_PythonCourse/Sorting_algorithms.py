4fc63cd97ffaea72f2067c3b1c63f36a8fdc967d
monkrobot <sdenisov93@mail.ru>
2018-09-10 17:59:34 +0300

    Coursera-MFTI_Timofei_Hirianov_PythonCourse - Sorting_algorithms.py (Lection 6)

---
+++ Coursera/MFTI_Timofei_Hirianov_PythonCourse/Sorting_algorithms.py
@@ -0,0 +1,76 @@
+#Алгоритм вставками
+def func1(N):
+    m = len(N)
+    for k in range(1, m):
+        for j in range(k, 0, -1):
+            if N[j] < N[j-1]:
+                N[j-1], N[j] = N[j], N[j-1]
+            else:
+                continue
+    return N
+
+#Алгоритм выбором
+def func2(N):
+    m = len(N)
+    for i in range(m-1):
+        for k in range(i+1, m):
+            if N[k] < N[i]:
+                N[i], N[k] = N[k], N[i]
+    return N
+
+#Алгоритм пузырек
+def func3(N):
+    m = len(N)
+    for k in range(m - 1):
+        for j in range(1, m - k):
+            if N[j] < N[j-1]:
+                N[j-1], N[j] = N[j], N[j-1]
+
+    return N
+
+def test_func(func):
+    N = [3,5,2,4,6]
+    ans = [2,3,4,5,6]
+    func_ans = func(N)
+    print("ok" if func_ans == ans else "fail")
+
+    N = [5, 5, 2, 6, 6, 3, 2]
+    ans = [2, 2, 3, 5, 5, 6, 6]
+    func_ans = func(N)
+    print("ok" if func_ans == ans else "fail")
+
+    N = list(range(10, 20)) + list(range(10))
+    ans = list(range(20))
+    func_ans = func(N)
+    print("ok" if func_ans == ans else "fail")
+
+if __name__ == "__main__":
+    for j in [func1, func2, func3]:
+        test_func(j)
+    print("_"*10)
+    print(func1(list(range(10, 20)) + list(range(10))))
+
+
+#решето Эратосфена
+import time
+
+start = time.clock()
+def func(N):
+    '''N - число, до которого надо найти все простые числа'''
+    assert N > 0, "Fail, N < 0"
+    prost = [True]*N
+    answer = [0, 1]
+    #prost[0], prost[1] = True, True
+    for k in range(2, len(prost)):
+        if prost[k]:
+            answer.append(k)
+            for m in range(2*k, N, k):
+                prost[m] = False
+        else:
+            continue
+    #print(prost)
+    print(len(answer))
+    print(time.clock() - start)
+
+
+func(-10)
--- Project_Euler/5_Smallest_multiple.py
+++ Project_Euler/5_Smallest_multiple.py
@@ -3,22 +3,25 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
-import time
+#import time
+#
+#start = time.clock()
+#N = 30
+## This solution is not good
+#for i in range(232792560, 10000000000000000, N):
+#
+#    for j in [4,7,8,9,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29]:
+#        #print('i:', i)
+#        if i % j == 0:
+#            continue
+#        else:
+#            #print('break on', j)
+#            break
+#    else:
+#        print('answer:', i)
+#        break
+#
+#print(time.clock() - start)
 
-start = time.clock()
 N = 30
-# This solution is not good
-for i in range(232792560, 10000000000000000, N):
 
-    for j in [4,7,8,9,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29]:
-        #print('i:', i)
-        if i % j == 0:
-            continue
-        else:
-            #print('break on', j)
-            break
-    else:
-        print('answer:', i)
-        break
-
-print(time.clock() - start)
