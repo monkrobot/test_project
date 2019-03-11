b4b0578d7e2b2efb108a34a0887e29871d4d3b43
monkrobot <sdenisov93@mail.ru>
2019-03-11 17:51:52 +0300

    Digital Design Test - finish test, problems with task3 - doesn't work with huge files

--- Digital_Design_Test/Task2.py
+++ Digital_Design_Test/Task2.py
@@ -18,45 +18,38 @@ Output:
 
 
 def compare_json(str1, str2):
-    try:
-        return str(a.items() - b.items()) + "\n" + str(b.items() - a.items())
-    except TypeError:
-        answer1 = []
-        answer2 = []
-        for val1, val2 in zip(str1.items(), str2.items()):
-            if val1 == val2:
-                continue
-            else:
-                print(str(val1[0]) + ":", val1[1])
-                print(str(val2[0]) + ":", val2[1])
-        if len(str1) < len(str2):
-            i = 1
-            for val in str2.items():
-                if i > len(str1):
-                    answer2.append({val[0]: val[1]})
-                    print('')
-                    print(str(val[0]) + ":", val[1])
-                i += 1
-        elif len(str2) < len(str1):
-            i = 1
-            for val in str1.items():
-                if i > len(str2):
-                    answer1.append({val[0]: val[1]})
-                    print(str(val[0]) + ":", val[1])
-                    print('')
-                i += 1
-        # return str(answer1) + '\n' + str(answer2)
+    '''
+    Compares 2 json strings
+    :param str1: json string 1
+    :param str2: json string 2
+    :return: print differences between json strings by each element
+    '''
+    for val1, val2 in zip(str1.items(), str2.items()):
+        if val1 == val2:
+            continue
+        else:
+            print(str(val1[0]) + ":", val1[1])
+            print(str(val2[0]) + ":", val2[1])
+    if len(str1) < len(str2):
+        i = 1
+        for val in str2.items():
+            if i > len(str1):
+                print('-')
+                print(str(val[0]) + ":", val[1])
+            i += 1
+    elif len(str2) < len(str1):
+        i = 1
+        for val in str1.items():
+            if i > len(str2):
+                print(str(val[0]) + ":", val[1])
+                print('-')
+            i += 1
+
 
 
 if __name__ == "__main__":
-    a = {"a": 2, "b": ["3", 3], "c": [{"a": 89}]}
-    b = {"a": 2, "b": 4}
-
-    #  = {"a": "hello", "c": 4, "b": {"c": 3}}
-    #  = {"a": "hello", "b": {"c": 3}, "c": 4}
-
-    # a = {"a": 2, "b": 3}
-    # b = {"a": 2, "b": 4}
+    a = {"a": 2, "b": ["3", 3], "m": {"k": 4}, "c": [{"a": 89}]}
+    b = {"a": 2, "b": 4, "m": {"k": 4}, "c": [{"a": 89, "b": 87}], "f": 12}
 
     compare_json(a, b)
 
--- Digital_Design_Test/Task3.py
+++ Digital_Design_Test/Task3.py
@@ -28,3 +28,34 @@ yyy
 👌👌👌
 👍👍👍
 '''
+
+
+# Doesn't work with huge files
+with open("a.txt", "r") as a, open("b.txt", "r") as b, open("c.txt", "w") as c:
+    file_list = []
+    for line in a:
+        file_list.append(line.rstrip())
+    for line in b:
+        file_list.append(line.rstrip())
+    file_list.sort()
+    text = '\n'.join(file_list)
+    c.write(text)
+
+
+
+
+#import pandas as pd
+## Doesn't work with huge files
+#with open("2008.txt", "r") as a, open("b.txt", "r") as b, open("c.txt", "w") as c:
+#    k = pd.read_csv("a.txt")
+#    #print(df)
+#    #list_a = list(a)
+#    #list_a[-1] = list_a[-1] + '\n'
+#    #list_b = list(b)
+#    #list_b[-1] = list_b[-1] + '\n'
+#    #file_list = list_a + list_b
+#    #file_list.sort()
+#    #print(file_list)
+#    #text = ''.join(file_list)
+#    k.sort([0])
+#    k.to_csv("c2.txt")
